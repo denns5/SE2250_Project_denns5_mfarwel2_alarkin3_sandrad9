fca1b3d6986f118f9186684786f11a7614ae566a
Derek Enns <denns5@uwo.ca>
2019-02-18 17:47:48 -0500

    Project, Phase 1, Step 1
    First Commit

---
+++ Phase 1/Assets/__Scripts/Hero.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour
+{
+    static public Hero S;
+
+    public float speed = 30;
+    public float rollMult = -45;
+    public float pitchMult = 30;
+
+    public float radius = 1f;
+    public float camWidth;
+    public float camHeight;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        camHeight = Camera.main.orthographicSize;
+        camWidth = camHeight * Camera.main.aspect;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void LateUpdate()
+    {
+        Vector3 pos = transform.position;
+        if (pos.x > camWidth - radius)
+        {
+            pos.x = camWidth - radius;
+        }
+        if (pos.x < -camWidth + radius)
+        {
+            pos.x = -camWidth + radius;
+        }
+        if (pos.y > camHeight - radius)
+        {
+            pos.y = camHeight - radius;
+        }
+        if (pos.y < -camHeight + radius)
+        {
+            pos.y = -camHeight + radius;
+        }
+        transform.position = pos;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xAxis = Input.GetAxis("Horizontal");
+        float yAxis = Input.GetAxis("Vertical");
+
+        Vector3 pos = transform.position;
+        pos.x += xAxis * speed * Time.deltaTime;
+        pos.y += yAxis * speed * Time.deltaTime;
+        transform.position = pos;
+
+        transform.rotation = Quaternion.Euler(yAxis * pitchMult, xAxis * rollMult, 0);
+    }
+}
