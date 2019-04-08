fcc08660acf6402d86e4a87aa83a3a73a2f8a8e0
Maddi Farwell <mfarwel2@uwo.ca>
2019-04-07 23:09:36 -0400

    Restart Button Script

---
+++ SHUMP Game/Assets/__Scripts/Restart_Button.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class RestartButton : MonoBehaviour
+{
+
+    public Button buttonComponent;
+
+    public float reloadDelay = 2f;// 2 sec delay between rounds
+
+    // Use this for initialization
+    void Start()
+    {
+        buttonComponent.onClick.AddListener(OnClick);
+    }
+
+
+    public void OnClick()
+    {
+        Invoke("ReloadLevel", reloadDelay);
+    }
+
+    void ReloadLevel()
+    {
+        // Reload the scene, resetting the game
+        SceneManager.LoadScene("__Prospector_Scene_0");
+    }
+
+}
